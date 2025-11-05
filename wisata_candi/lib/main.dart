dcbe3fd730cf23a2c87448f0d19d02a39268651e
Jesslyn Kimiko <jesslynkimiko_2428250005@mhs.mdp.ac.id>
2025-11-05 11:24:17 +0700

    2

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(candi:candiList[0]),
+      home: ProfileScreen(),
+      //home: DetailScreen(candi:candiList[0]),
     );
   }
 }
